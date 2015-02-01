0783da69cc94a38bb1764831e915cf4e1d7af515
Indy <dev@indydedeken.fr>
2015-02-01 19:09:26 +0100

    Modification de l'URL allocine
    récupération des films présents dans le cinema souhaité.

--- allocine.js
+++ allocine.js
@@ -1,20 +1,20 @@
 exports.action = function(data, callback, config, SARAH){
 
-  var place = data.place || 'B0172';
-  
-  var url = 'http://mobile.allocine.fr/salle/seances_gen_csalle='+place+'.html';
+  var place = data.place || 'B0059';
+
+  var url = 'http://www.allocine.fr/seance/salle_gen_csalle='+place+'.html';
   var request = require('request');
   request({ 'uri' : url }, function (err, response, body){
     
-    if (err || response.statusCode != 200) {
-      callback({'tts': "L'action a échoué"});
+    if(err || response.statusCode != 200) {
+      callback({'tts': "L'action a échoué. Erreur " + response.statusCode});
       return;
     }
-    
+
     var $ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
     var options = list($);
     options.tts = 'Voici la liste des films au ' + options.theatre + ': ' + options.movies.join(', ');
-     
+
     if (data.movie){
       options.hours = hours($, data.movie);
       options.tts   = 'Voici les horaires de '+ options.movies[data.movie] +' au ' + options.theatre + ': ';
@@ -22,7 +22,7 @@ exports.action = function(data, callback, config, SARAH){
     } else {
       update(data.directory, options.movies, place);
     }
-   
+
     callback(options);
   });
 }
@@ -46,8 +46,8 @@ if (!String.prototype.encodeHTML) {
   // ------------------------------------------
 
 var list = function($){
-  var theatre = $('DIV.titre B').text();
-  var movies  = $('DIV.cell A[href^="/film"]').map(function(){ return $(this).text(); });
+  var theatre = $('DIV.boxbasichdr span').text();
+  var movies  = $('DIV.line A[href^="#movie"]').map(function(){ return $(this).text(); });
   return {
     'theatre' : theatre,
     'movies'  : movies
@@ -73,16 +73,16 @@ var clean = function(hours){
 var update = function(directory, movies, place){
   if (!directory){ return; }
   if (!movies || movies.length == 0){ return; }
-  
+
   var fs   = require('fs');
   var file = directory + '/../plugins/allocine/allocine.xml';
   var xml  = fs.readFileSync(file,'utf8');
-  
+
   var replace  = '§ -->\n';
       replace += '<rule id="ruleMovieName">\n';
       replace += '  <tag>out.place="'+place+'";</tag>\n';
       replace += '  <one-of>\n';
-      
+
   for(var i = 0 ; i < movies.length ; i++){
       var movie =  movies[i]; movie = movie.indexOf(':') > 0 ? movie.substring(0,movie.indexOf(':')) : movie; // Split at ':'
       replace += '    <item>'+movie.encodeHTML()+'<tag>out.movie="'+i+'";</tag></item>\n';
@@ -90,10 +90,9 @@ var update = function(directory, movies, place){
       replace += '  </one-of>\n';
       replace += '</rule>\n';
       replace += '<!-- §';
-  
+
   var regexp = new RegExp('§[^§]+§','gm');
   var xml    = xml.replace(regexp,replace);
-  
+
   fs.writeFileSync(file, xml, 'utf8');
 }
-  
